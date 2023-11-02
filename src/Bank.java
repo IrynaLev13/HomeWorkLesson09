6c697b1f0bddb9be8f751639fc56c9c4ab8ea7c7
IrynaLev13 <ikot@ukr.net>
2023-11-02 19:49:25 +0200

    HW17Rate First commit

---
+++ src/Bank.java
@@ -0,0 +1,30 @@
+public class Bank {
+    public String nameBank;
+        public double rate;
+
+    public Bank(String nameBank, double rate) {
+        this.nameBank = nameBank;
+        this.rate = rate;
+        System.out.println(this.nameBank + " " + " " + this.rate);
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/HW17Currency.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class HW17Currency {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        Bank bank01 = new Bank("Приват", 5 * random.nextDouble() + 35);
+        Bank bank02 = new Bank("АВАЛЬ", 5 * random.nextDouble() + 35);
+        Bank bank03 = new Bank("Сенс", 5 * random.nextDouble() + 35);
+        Bank bank04 = new Bank("ТАСКОМБАНК", 5 * random.nextDouble() + 35);
+        Bank bank05 = new Bank("Універсал", 5 * random.nextDouble() + 35);
+        Bank bank06 = new Bank("Ощадбанк", 5 * random.nextDouble() + 35);
+        Bank bank07 = new Bank("Укрексімбанк", 5 * random.nextDouble() + 35);
+        Bank bank08 = new Bank("УКРГАЗБАНК", 5 * random.nextDouble() + 35);
+        Bank bank09 = new Bank("Райффайзен ", 5 * random.nextDouble() + 35);
+        Bank bank10 = new Bank("УКРСИББАНК", 5 * random.nextDouble() + 35);
+        Bank bank11 = new Bank("ПУМБ", 5 * random.nextDouble() + 35);
+        Bank bank12 = new Bank("Південний", 5 * random.nextDouble() + 35);
+        Bank bank13 = new Bank("КРЕДОБАНК", 5 * random.nextDouble() + 35);
+        Bank bank14 = new Bank("ТАС", 5 * random.nextDouble() + 35);
+
+        double [] array01 = {bank01.rate, bank02.rate, bank03.rate, bank04.rate, bank05.rate, bank06.rate,bank07.rate, bank08.rate, bank09.rate, bank10.rate, bank11.rate, bank12.rate, bank13.rate, bank14.rate};
+        // System.out.println(Arrays.toString(array01));
+        double max = array01 [0];
+        for (int i = 1; i< array01.length; i++) max = Math.max(max, array01[i]);
+
+        System.out.println("The most profitable course today: " + " "+ max);
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+
