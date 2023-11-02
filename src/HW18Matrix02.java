213d6b3a239b76caa158c5aef34a525aa5204cf1
IrynaLev13 <ikot@ukr.net>
2023-11-02 21:41:31 +0200

    HW17Rate +HW18

---
+++ src/HW18Matrix02.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class HW18Matrix02 {
+    public static void main(String[] args) {
+
+        int[][] arr2D = {{1, 2, 3, 4, 5, 6, 7, 8},{8, 7, 6, 5, 4, 3, 2, 1}, {1, 2, 3, 4, 5, 6, 7, 8}, {8, 7, 6, 5, 4, 3, 2, 1}, {1, 2, 3, 4, 5, 6, 7, 8},{8, 7, 6, 5, 4, 3, 2, 1}, {1, 2, 3, 4, 5, 6, 7, 8}, {8, 7, 6, 5, 4, 3, 2, 1}};
+        for (int i = 0; i < 8; i++){
+            for (int j = 0; j<8; j++) {
+                System.out.print(" " + arr2D[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+
+
+    }
+}
